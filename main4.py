330bee2bb58cd667cd423f0d964048a43e4c4e11
Jadan Ercoli <jtercoli@iu.edu>
2019-09-04 01:26:35 -0400

    Updates LICENSE, README.md, and main.py(1-4)

--- main4.py
+++ main4.py
@@ -13,15 +13,15 @@ arcade.start_render()
 
 
 #start at 100, go to 799, counting by 150
-for x in range(100,800,150):
+for x in range(100,800,100):
         #start at 100, go to 599, counting by 150
-        for y in range(100,600,150):
+        for y in range(200,600,200):
                 face_x,face_y = (x,y)
-                smile_x,smile_y = (face_x + 0,face_y - 0)
-                eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-                eye2_x,eye2_y = (face_x + 0,face_y + 0)
-                catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-                catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+                smile_x,smile_y = (face_x + 0,face_y - 10)
+                eye1_x,eye1_y = (face_x - 30,face_y + 40) 
+                eye2_x,eye2_y = (face_x + 30,face_y + 40)
+                catch1_x,catch1_y = (face_x - 27,face_y + 45) 
+                catch2_x,catch2_y = (face_x + 33,face_y + 45) 
 
 
                 # Draw the smiley face:
